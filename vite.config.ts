d2b68227d395164035efa61692a4351a60ddb37a
Zhuoheng Huang <84053205+salvere002@users.noreply.github.com>
2025-05-18 23:49:53 -0400

    vite proxy for all requests

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createProxyConfig } from './src/proxy/proxyConfig';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000, // Optional: Set a specific port for the dev server
+    proxy: createProxyConfig(),
   },
   build: {
-    outDir: 'dist', // Match our previous tsconfig setting
+    outDir: 'dist',
   },
 }); 
